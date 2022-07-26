46feaebe359bedeecdf985ad355c3a9a6054a135
chidimi <daichi.imai@hotmail.com>
2022-07-27 01:25:01 +0900

    add base authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './auth.local.strategy';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
